44324043ea8111cddd981305a1c06fe09b2d0d71
Rai <s1.ranjan@gmail.com>
2026-02-22 20:48:10 +0530

    Refactor namespace and update route definitions

--- extend.php
+++ extend.php
@@ -1,31 +1,13 @@
 <?php
 
-namespace Dhtml\FlarumContentPages;
+namespace Dhtml\ContentPages;
 
 use Flarum\Extend;
-use Dhtml\FlarumContentPages\Api\Controllers\DataApiController;
-use Dhtml\FlarumContentPages\Api\Controllers\PagesApiController;
-use Dhtml\FlarumContentPages\Providers\LocaleServiceProvider;
+use Dhtml\ContentPages\Controller\ShowPageController;
 
 return [
-    (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/resources/less/forum.less')
-        ->route('/about-us', 'about-us')
-        ->route('/contact-us', 'contact-us')
-        ->route('/terms', 'terms')
-        ->route('/guidelines', 'guidelines')
-        ->route('/privacy-policy', 'privacy-policy')
-        ->route('/download', 'download'),
 
-    new Extend\Locales(__DIR__.'/locale'),
+    (new Extend\Routes('forum'))
+        ->get('/{slug:[a-z0-9\-]+}', 'content-pages.page', ShowPageController::class),
 
-    (new Extend\Routes('api'))
-        ->get('/cpage/{slug}', 'cpages.load', PagesApiController::class),
-
-    (new Extend\Routes('api'))
-        ->get('/cpages-data', 'cpages.data', DataApiController::class),
-
-    (new Extend\ServiceProvider())
-        ->register(LocaleServiceProvider::class),
 ];
